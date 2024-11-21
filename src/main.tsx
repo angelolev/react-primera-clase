ba0561211d9c7d6a8a4ccc67d0483762dd76c12f
Angelo Leva <angelokta7@gmail.com>
2024-11-20 20:05:19 -0500

    clase 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import ThemeSwitcher from "./DarkMode.tsx";
+import WelcomeMessage from "./Message.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<WelcomeMessage />);
